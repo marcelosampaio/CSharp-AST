65f9b5e135787302c1e16ba937aee1c33ae0ef97
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-19 13:46:25 -0300

    feat: Exercicio 1

---
+++ CSharpAT01/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, meu nome é Marcelo!");
+        Console.WriteLine("Nasci em 65 e estou aprendendo C#!");
+    }
+}
