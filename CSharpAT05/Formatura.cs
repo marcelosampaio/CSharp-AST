c0129dc55c3a196356008adb32a5485a546a8729
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-19 19:45:39 -0300

    wip: classe Formatura e estrutura da classe Program

---
+++ CSharpAT05/Formatura.cs
@@ -0,0 +1,11 @@
+namespace CSharpAT05;
+
+public class Formatura
+{
+    private readonly DateTime dataFormatura;
+
+    public Formatura(DateTime dataFormatura)
+    {
+        this.dataFormatura = dataFormatura;
+    }
+}
---
+++ CSharpAT05/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpAT05;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime dataFormatura = new DateTime(2026, 12, 15);
+
+        Formatura formatura = new Formatura(dataFormatura);
+
+        Console.WriteLine("checkpoint commit");
+    }
+}
